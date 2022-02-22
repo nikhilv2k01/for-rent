d184706e2f5527f18be42185a62896761da0d9cb
Arun151996 <arun.akds@gmail.com>
2022-02-22 23:35:41 +0530

    hom

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import path
 urlpatterns=[
-    path('',views.Home)
+    path('',views.Home),
+    path('sg',views.sign , name="sin")
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     return render(request,"d.html")
+def sign(request):
+    return render(request,"e.html")
