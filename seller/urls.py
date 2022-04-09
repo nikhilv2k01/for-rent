c428656e86cf64f0b5cf60ca4e5d313a930d5bec
Arun151996 <arun.akds@gmail.com>
2022-04-10 00:49:43 +0530

    payment page

--- seller/urls.py
+++ seller/urls.py
@@ -1,5 +1,8 @@
 from . import views
 from django.urls import path
 urlpatterns=[
-    path('app2',views.home)
+    path('house',views.home),
+    path('shop',views.shop,name="sh"),
+    path('pack',views.pack),
+    path('pay',views.pay)
 ]
